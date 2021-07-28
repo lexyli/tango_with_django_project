a63e85c0956fcc78fc264a4b1880a859970186f6
Lexin Li <2603829l@student.gla.ac.uk>
2021-07-28 21:36:57 +0100

    add meta

--- populate_rango.py
+++ populate_rango.py
@@ -51,6 +51,8 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
