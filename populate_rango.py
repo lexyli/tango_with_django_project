e56156845ac5808683247738660c0fd0c5d402a7
Lexin Li <2603829l@student.gla.ac.uk>
2021-07-28 21:58:12 +0100

    change number of views and likes

--- populate_rango.py
+++ populate_rango.py
@@ -29,12 +29,12 @@ def populate():
         {'title':'Flask',
          'url':'http://flask.pocoo.org'}]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -51,8 +51,8 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name):
     c = Category.objects.get_or_create(name=name)[0]
-    # c.views = views
-    # c.likes = likes
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
