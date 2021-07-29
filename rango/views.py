4e12abc2e1bea6cbc523631d529cd8620204540d
Lexin Li <2603829l@student.gla.ac.uk>
2021-07-29 01:53:11 +0100

    change view.py

--- rango/views.py
+++ rango/views.py
@@ -5,10 +5,13 @@ from rango.models import Category
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    context_dict['extra'] = 'From the model solution on Github'
     
     return render(request, 'rango/index.html', context=context_dict)
 
