2058dc9b25510dd4d27758ad40ffdedfe136798b
Lexin Li <2603829l@student.gla.ac.uk>
2021-07-29 02:16:01 +0100

    fix

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
-from rango.models import Page
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
