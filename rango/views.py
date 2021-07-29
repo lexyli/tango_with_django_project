f21e0fac1a6196329de6905a9624142ab8e5422f
Lexin Li <2603829l@student.gla.ac.uk>
2021-07-29 22:41:36 +0100

    fix

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from rango.models import Category, Page 
-from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -15,8 +14,8 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['extra'] = 'From the model solution on Github'
-    
+    context_dict['extra'] = 'From the model solution on GitHub'
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -27,16 +26,17 @@ def show_category(request, category_name_slug):
 
     try:
         category = Category.objects.get(slug=category_name_slug)
-
         pages = Page.objects.filter(category=category)
+
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
-        context_dict['category'] = None
         context_dict['pages'] = None
-
+        context_dict['category'] = None
+    
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -48,18 +48,22 @@ def add_category(request):
             return redirect(reverse('rango:index'))
         else:
             print(form.errors)
+    
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
-    except Category.DoesNotExist:
+    except:
         category = None
-
+    
+    
     if category is None:
-        return redirect('rango:index')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
+
     if request.method == 'POST':
         form = PageForm(request.POST)
 
@@ -70,13 +74,10 @@ def add_page(request, category_name_slug):
                 page.views = 0
                 page.save()
 
-                return redirect(reverse('rango:show_category',
-                                        kwargs={'category_name_slug':
-                                                 category_name_slug} ))
-
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors)
-
+            print(form.errors)  
+    
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
@@ -106,11 +107,7 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
     
-    return render(request, 
-                  'rango/register.html', 
-                  context={'user_form': user_form, 
-                           'profile_form': profile_form, 
-                           'registered': registered})
+    return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 def user_login(request):
     if request.method == 'POST':
@@ -131,9 +128,11 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
+@login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
 
+@login_required
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
