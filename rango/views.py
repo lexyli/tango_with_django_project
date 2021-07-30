eab18d98139e8de3258d2b3a3cd2f6b1add9758b
Lexin Li <2603829l@student.gla.ac.uk>
2021-07-30 01:14:14 +0100

    test cookie

--- rango/views.py
+++ rango/views.py
@@ -16,9 +16,14 @@ def index(request):
     context_dict['pages'] = page_list
     context_dict['extra'] = 'From the model solution on GitHub'
 
+    request.session.set_test_cookie()
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
